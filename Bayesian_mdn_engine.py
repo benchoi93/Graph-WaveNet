a94ac54e2fc95cf978b9d2e1808c2e9de5fb23d9
benchoi93 <benchoi93@gmail.com>
2022-05-12 16:09:15 -0400

    update bayes

---
+++ Bayesian_mdn_engine.py
@@ -0,0 +1,283 @@
+import torch.optim as optim
+from model import *
+import util
+import properscoring as ps
+
+import seaborn as sns
+
+import torch.nn as nn
+import torch.distributions as Dist
+
+from tensorboardX import SummaryWriter
+
+
+class LowRankMDNhead(nn.Module):
+    def __init__(self, n_components, n_vars, n_rank, pred_len=12):
+
+        self.n_components = n_components
+        self.n_vars = n_vars
+        self.n_rank = n_rank
+        self.pred_len = pred_len
+
+        self.dim_w = n_components
+        self.dim_mu = n_components * n_vars
+        self.dim_D = n_components * n_vars
+        self.dim_V = n_components * n_vars * n_rank
+
+        self.output_dim = self.dim_w + self.dim_mu + self.dim_D + self.dim_V
+
+    def forward(self, features, y):
+        # input : features = dict of tensors
+        #    - features['w'] : (batch_size, n_components)
+        #    - features['mu'] : (batch_size, n_components, n_vars)
+        #    - features['D'] : (batch_size, n_components, n_vars)
+        #    - features['V'] : (batch_size, n_components, n_vars, n_rank)
+
+        # check if 'w' , 'mu' , 'D' and 'V' are in features.keys()
+        # raise error if not
+        assert('w' in features.keys())
+        assert('mu' in features.keys())
+        assert('D' in features.keys())
+        assert('V' in features.keys())
+
+        dist = self.get_output_distribution(features)
+        loss = - dist.log_prob(y[:, :, self.pred_len - 1]).mean()
+        return loss
+
+    def sample(self, features, n=None):
+        dist = self.get_output_distribution(features)
+
+        if n is None:
+            return dist.sample()
+        else:
+            return dist.sample((n,))
+
+    def get_output_distribution(self, features):
+        # input : features
+        # shape of input = (batch_size, hidden)
+        w, mu, D, V = self.get_parameters(features)
+        mix_dist = Dist.Categorical(w.squeeze(-1))
+        com_dist = Dist.LowRankMultivariateNormal(
+            loc=mu,
+            cov_factor=V,
+            # cov_diag=torch.ones_like(D) * 0.01
+            cov_diag=D + 1e-4
+        )
+        # com_dist = Dist.MultivariateNormal(
+        #     loc=mu,
+        #     covariance_matrix=torch.diag_embed(D+1e-6)
+        # )
+
+        dist = Dist.MixtureSameFamily(mix_dist, com_dist)
+        return dist
+
+    def get_parameters(self, features):
+        # input : features : dict of tensors, keys: w, mu, D, V
+        return features['w'], features['mu'], features['D'], features['V']
+
+
+class LowRankMDN(nn.Module):
+    def __init__(self, n_components, n_vars, n_rank, pred_len=12):
+        super().__init__()
+        self.n_components = n_components
+        self.n_vars = n_vars
+        self.n_rank = n_rank
+        self.pred_len = pred_len
+
+        self.D = nn.Parameter(torch.randn(
+            n_components, n_vars), requires_grad=True)
+        self.V = nn.Parameter(torch.randn(
+            n_components, n_vars, n_rank), requires_grad=True)
+        self.mu = nn.Parameter(torch.randn(
+            n_components, n_vars), requires_grad=True)
+
+
+class MDN_trainer():
+    def __init__(self, scaler, in_dim, seq_length, num_nodes, num_rank, nhid, dropout, lrate, wdecay, device, supports, gcn_bool, addaptadj, aptinit, n_components):
+
+        self.num_nodes = num_nodes
+        self.n_components = n_components
+        self.num_rank = num_rank
+
+        self.device = device
+
+        self.dim_w = n_components
+        self.dim_mu = n_components * num_nodes
+        self.dim_V = n_components * num_nodes * num_rank
+        self.dim_D = n_components * num_nodes
+
+        # dim_out = self.dim_w + self.dim_mu + self.dim_V + self.dim_D
+        self.out_per_comp = nhid
+        dim_out = n_components * self.out_per_comp
+
+        self.model = gwnet(device, num_nodes, dropout, supports=supports, gcn_bool=gcn_bool, addaptadj=addaptadj, aptinit=aptinit,
+                           in_dim=in_dim, out_dim=dim_out, residual_channels=nhid, dilation_channels=nhid, skip_channels=nhid * 8, end_channels=nhid * 16)
+        self.mdn_head = LowRankMDNhead(n_components, num_nodes, num_rank)
+        self.mdn = LowRankMDN(n_components, num_nodes, num_rank)
+
+        # dim_w = [batn_components]
+        # dims_c = dim_w, dim_mu, dim_U_entries, dim_i
+        # self.mdn = FrEIA.modules.GaussianMixtureModel(dims_in, dims_c)
+
+        self.fc_w = nn.Sequential(
+            nn.Linear(num_nodes*self.out_per_comp, nhid),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(nhid, nhid),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(nhid, 1)
+        )
+
+        self.model.to(device)
+        # self.mdn_head.to(device)
+        self.fc_w.to(device)
+        self.mdn.to(device)
+
+        self.optimizer = optim.Adam(
+            list(self.model.parameters()) + list(self.mdn.parameters()) + list(self.fc_w.parameters()), lr=lrate, weight_decay=wdecay)
+        self.loss = util.masked_mae
+        self.scaler = scaler
+        self.clip = 5
+
+        import datetime
+        self.logdir = f'./logs/GWN_MDN_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}'
+        self.summary = SummaryWriter(logdir=f'{self.logdir}')
+        self.cnt = 0
+
+    def train(self, input, real_val):
+        self.model.train()
+        self.optimizer.zero_grad()
+        input = nn.functional.pad(input, (1, 0, 0, 0))
+        output = self.model(input)
+        output = output.transpose(1, 3)
+
+        output = output.view(-1, self.num_nodes,
+                             self.n_components, self.out_per_comp)
+
+        mus = (self.mdn.mu).unsqueeze(0).repeat(input.size(0), 1, 1)
+        D = nn.functional.elu(self.mdn.D) + 1
+        D = D.unsqueeze(0).repeat(input.size(0), 1, 1)
+        V = (self.mdn.V).unsqueeze(0).repeat(input.size(0), 1, 1, 1)
+
+        #    - features['w'] : (batch_size, n_components)
+        #    - features['mu'] : (batch_size, n_components, n_vars)
+        #    - features['D'] : (batch_size, n_components, n_vars)
+        #    - features['V'] : (batch_size, n_components, n_vars, n_rank)
+
+        output = torch.einsum("bijk->bjik", output)
+        output = output.reshape(-1, self.n_components,
+                                self.num_nodes * self.out_per_comp)
+        w = self.fc_w(output)
+        # w activate softmax0
+        w = nn.functional.softmax(w, dim=1)
+
+        scaled_real_val = self.scaler.transform(real_val)
+        loss = self.mdn_head.forward(
+            features={'w': w, 'mu': mus, 'D': D, 'V': V}, y=scaled_real_val)
+        loss.backward()
+        if self.clip is not None:
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip)
+        self.optimizer.step()
+        # mape = util.masked_mape(predict, real, 0.0).item()
+        # rmse = util.masked_rmse(predict, real, 0.0).item()
+        real = real_val[:, :, 11]
+        output = self.mdn_head.sample(
+            features={'w': w, 'mu': mus, 'D': D, 'V': V})
+        predict = self.scaler.inverse_transform(output)
+        mape = util.masked_mape(predict, real, 0.0).item()
+        rmse = util.masked_rmse(predict, real, 0.0).item()
+
+        return loss.item(), mape, rmse
+
+    def eval(self, input, real_val):
+        if self.model.training:
+            self.specific = True
+
+        self.model.eval()
+        input = nn.functional.pad(input, (1, 0, 0, 0))
+        output = self.model(input)
+        output = output.transpose(1, 3)
+        output = output.view(-1, self.num_nodes,
+                             self.n_components, self.out_per_comp)
+        mus = (self.mdn.mu).unsqueeze(0).repeat(input.size(0), 1, 1)
+        D = nn.functional.elu(self.mdn.D) + 1
+        D = D.unsqueeze(0).repeat(input.size(0), 1, 1)
+        V = (self.mdn.V).unsqueeze(0).repeat(input.size(0), 1, 1, 1)
+
+        #    - features['w'] : (batch_size, n_components)
+        #    - features['mu'] : (batch_size, n_components, n_vars)
+        #    - features['D'] : (batch_size, n_components, n_vars)
+        #    - features['V'] : (batch_size, n_components, n_vars, n_rank)
+
+        output = torch.einsum("bijk->bjik", output)
+        output = output.reshape(-1, self.n_components,
+                                self.num_nodes * self.out_per_comp)
+        w = self.fc_w(output)
+        # w activate softmax0
+        w = nn.functional.softmax(w, dim=1)
+
+        scaled_real_val = self.scaler.transform(real_val)
+        loss = self.mdn_head.forward(
+            features={'w': w, 'mu': mus, 'D': D, 'V': V}, y=scaled_real_val)
+
+        # output = self.model(input)
+        # output = output.transpose(1, 3)
+        #output = [batch_size,12,num_nodes,1]
+        # real = torch.unsqueeze(real_val, dim=1)
+        real = real_val[:, :, 11]
+        output = self.mdn_head.sample(
+            features={'w': w, 'mu': mus, 'D': D, 'V': V})
+        predict = self.scaler.inverse_transform(output)
+        # predict = output
+        # loss = self.loss(predict, real, 0.0)
+        mape = util.masked_mape(predict, real, 0.0).item()
+        rmse = util.masked_rmse(predict, real, 0.0).item()
+
+        if self.specific:
+            self.specific_eval(
+                features={'w': w, 'mu': mus, 'D': D, 'V': V}, y=real_val)
+
+        return loss.item(), mape, rmse
+
+    def specific_eval(self, features, y):
+        self.specific = False
+        self.cnt += 1
+        w = features['w']
+        mu = features['mu']
+        D = features['D']
+        V = features['V']
+
+        output = self.mdn_head.sample(
+            features={'w': w, 'mu': mu, 'D': D, 'V': V}, n=1000)
+        # pred = self.scaler.inverse_transform(output)
+        real_val = y[:, 11, :]
+        # real_val = real_val.expand_as(output)
+
+        crps = torch.zeros(size=(y.shape[0], y.shape[2]))
+        for i in range(y.shape[0]):
+            for j in range(y.shape[2]):
+                pred = self.scaler.inverse_transform(
+                    output[:, i, j]).cpu().numpy()
+                crps[i, j] = ps.crps_ensemble(
+                    real_val[i, j].cpu().numpy(), pred)
+
+        self.summary.add_scalar('val/crps', crps.mean().item(), self.cnt)
+
+        dist = self.mdn_head.get_output_distribution(features)
+        sample_cov = dist.component_distribution.covariance_matrix[0]
+
+        corr = torch.zeros_like(sample_cov)
+        for i in range(sample_cov.size(0)):
+            corr[i] = torch.corrcoef(sample_cov[i])
+
+        for i in range(sample_cov.shape[0]):
+            sns_plot = sns.heatmap(corr[i].detach().cpu(
+            ).numpy(), cmap='coolwarm', vmin=-1, vmax=1)
+            fig = sns_plot.get_figure()
+            self.summary.add_figure('corr_matrix/' + str(i), fig,  self.cnt)
+
+            sns_plot = sns.heatmap(
+                sample_cov[i].detach().cpu().numpy(), cmap='coolwarm')
+            fig = sns_plot.get_figure()
+            self.summary.add_figure('cov_matrix/' + str(i), fig,  self.cnt)
--- train.py
+++ train.py
@@ -5,31 +5,44 @@ import time
 import util
 import matplotlib.pyplot as plt
 from engine import trainer
-from mdn_engine import MDN_trainer
+# from mdn_engine import MDN_trainer
+from Bayesian_mdn_engine import MDN_trainer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--device', type=str, default='cuda:0', help='')
-parser.add_argument('--data', type=str, default='data/PEMS-BAY', help='data path')
-parser.add_argument('--adjdata', type=str, default='data/sensor_graph/adj_mx_bay.pkl', help='adj data path')
-parser.add_argument('--adjtype', type=str, default='doubletransition', help='adj type')
-parser.add_argument('--gcn_bool', action='store_true', help='whether to add graph convolution layer')
-parser.add_argument('--aptonly', action='store_true', help='whether only adaptive adj')
-parser.add_argument('--addaptadj', action='store_true', help='whether add adaptive adj')
-parser.add_argument('--randomadj', action='store_true', help='whether random initialize adaptive adj')
+parser.add_argument('--data', type=str,
+                    default='data/PEMS-BAY', help='data path')
+parser.add_argument('--adjdata', type=str,
+                    default='data/sensor_graph/adj_mx_bay.pkl', help='adj data path')
+parser.add_argument('--adjtype', type=str,
+                    default='doubletransition', help='adj type')
+parser.add_argument('--gcn_bool', action='store_true',
+                    help='whether to add graph convolution layer')
+parser.add_argument('--aptonly', action='store_true',
+                    help='whether only adaptive adj')
+parser.add_argument('--addaptadj', action='store_true',
+                    help='whether add adaptive adj')
+parser.add_argument('--randomadj', action='store_true',
+                    help='whether random initialize adaptive adj')
 parser.add_argument('--seq_length', type=int, default=12, help='')
 parser.add_argument('--num-rank', type=int, default=5, help='')
 parser.add_argument('--nhid', type=int, default=64, help='')
 parser.add_argument('--in_dim', type=int, default=2, help='inputs dimension')
-parser.add_argument('--num_nodes', type=int, default=12, help='number of nodes')
-parser.add_argument('--n-components', type=int, default=5, help='number of mixture components')
+parser.add_argument('--num_nodes', type=int,
+                    default=12, help='number of nodes')
+parser.add_argument('--n-components', type=int, default=20,
+                    help='number of mixture components')
 parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
+parser.add_argument('--learning_rate', type=float,
+                    default=0.001, help='learning rate')
 parser.add_argument('--dropout', type=float, default=0.3, help='dropout rate')
-parser.add_argument('--weight_decay', type=float, default=0.0001, help='weight decay rate')
+parser.add_argument('--weight_decay', type=float,
+                    default=0.0001, help='weight decay rate')
 parser.add_argument('--epochs', type=int, default=100000, help='')
 parser.add_argument('--print_every', type=int, default=50, help='')
 #parser.add_argument('--seed',type=int,default=99,help='random seed')
-parser.add_argument('--save', type=str, default='./garage/pems', help='save path')
+parser.add_argument('--save', type=str,
+                    default='./garage/pems', help='save path')
 parser.add_argument('--expid', type=int, default=1, help='experiment id')
 
 args = parser.parse_args()
@@ -41,7 +54,8 @@ def main():
     # np.random.seed(args.seed)
     # load data
     device = torch.device(args.device)
-    sensor_ids, sensor_id_to_ind, adj_mx = util.load_adj(args.adjdata, args.adjtype)
+    sensor_ids, sensor_id_to_ind, adj_mx = util.load_adj(
+        args.adjdata, args.adjtype)
 
     target_sensors = ['404444',
                       '404434',
@@ -61,7 +75,8 @@ def main():
 
     target_sensor_inds = [sensor_id_to_ind[i] for i in target_sensors]
 
-    dataloader = util.load_dataset(args.data, args.batch_size, args.batch_size, args.batch_size, target_sensor_inds=target_sensor_inds)
+    dataloader = util.load_dataset(args.data, args.batch_size, args.batch_size,
+                                   args.batch_size, target_sensor_inds=target_sensor_inds)
     scaler = dataloader['scaler']
     supports = [torch.tensor(i).to(device) for i in adj_mx]
 
@@ -110,7 +125,8 @@ def main():
             train_rmse.append(metrics[2])
             if iter % args.print_every == 0:
                 log = 'Iter: {:03d}, Train Loss: {:.4f}, Train MAPE: {:.4f}, Train RMSE: {:.4f}'
-                print(log.format(iter, train_loss[-1], train_mape[-1], train_rmse[-1]), flush=True)
+                print(log.format(
+                    iter, train_loss[-1], train_mape[-1], train_rmse[-1]), flush=True)
         t2 = time.time()
         train_time.append(t2-t1)
         # validation
@@ -149,14 +165,18 @@ def main():
         engine.summary.add_scalar('errors/val_rmse', mvalid_rmse, i)
 
         log = 'Epoch: {:03d}, Train Loss: {:.4f}, Train MAPE: {:.4f}, Train RMSE: {:.4f}, Valid Loss: {:.4f}, Valid MAPE: {:.4f}, Valid RMSE: {:.4f}, Training Time: {:.4f}/epoch'
-        print(log.format(i, mtrain_loss, mtrain_mape, mtrain_rmse, mvalid_loss, mvalid_mape, mvalid_rmse, (t2 - t1)), flush=True)
-        torch.save(engine.model.state_dict(), args.save+"_epoch_"+str(i)+"_"+str(round(mvalid_loss, 2))+".pth")
-    print("Average Training Time: {:.4f} secs/epoch".format(np.mean(train_time)))
+        print(log.format(i, mtrain_loss, mtrain_mape, mtrain_rmse,
+              mvalid_loss, mvalid_mape, mvalid_rmse, (t2 - t1)), flush=True)
+        torch.save(engine.model.state_dict(), args.save+"_epoch_" +
+                   str(i)+"_"+str(round(mvalid_loss, 2))+".pth")
+    print(
+        "Average Training Time: {:.4f} secs/epoch".format(np.mean(train_time)))
     print("Average Inference Time: {:.4f} secs".format(np.mean(val_time)))
 
     # testing
     bestid = np.argmin(his_loss)
-    engine.model.load_state_dict(torch.load(args.save+"_epoch_"+str(bestid+1)+"_"+str(round(his_loss[bestid], 2))+".pth"))
+    engine.model.load_state_dict(torch.load(
+        args.save+"_epoch_"+str(bestid+1)+"_"+str(round(his_loss[bestid], 2))+".pth"))
 
     outputs = []
     realy = torch.Tensor(dataloader['y_test']).to(device)
@@ -190,7 +210,8 @@ def main():
 
     log = 'On average over 12 horizons, Test MAE: {:.4f}, Test MAPE: {:.4f}, Test RMSE: {:.4f}'
     print(log.format(np.mean(amae), np.mean(amape), np.mean(armse)))
-    torch.save(engine.model.state_dict(), args.save+"_exp"+str(args.expid)+"_best_"+str(round(his_loss[bestid], 2))+".pth")
+    torch.save(engine.model.state_dict(), args.save+"_exp" +
+               str(args.expid)+"_best_"+str(round(his_loss[bestid], 2))+".pth")
 
 
 if __name__ == "__main__":
