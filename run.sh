7e9171c2b78be77c6ecf416ef85934d90e531ee1
benchoi93 <benchoi93@gmail.com>
2023-02-01 14:54:39 +0000

    include wandb in gitignore

--- run.sh
+++ run.sh
@@ -14,10 +14,10 @@
 # CUDA_VISIBLE_DEVICES=3 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 1000 --batch_size 64 --num-rank 4 --nhid 32  --reg_coef 0 --mse_coef 1 --rho 0.01  --nonlinearity softplus  >> out_run24.log &
 
 
-CUDA_VISIBLE_DEVICES=2 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 5 --nhid 64  --reg_coef 0 --mse_coef 1 --rho 0.01 --loss mae >> out_run01.log &
-CUDA_VISIBLE_DEVICES=3 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 5 --nhid 64  --reg_coef 0 --mse_coef 1 --rho 0.001 --loss mae  >> out_run02.log &
-CUDA_VISIBLE_DEVICES=2 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 5 --nhid 64  --reg_coef 0 --mse_coef 1 --rho 0.01 --loss mse  >> out_run03.log &
-CUDA_VISIBLE_DEVICES=3 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 5 --nhid 64  --reg_coef 0 --mse_coef 1 --rho 0.001 --loss mse  >> out_run04.log &
+CUDA_VISIBLE_DEVICES=0 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 1 --nhid 64  --reg_coef 0 --mse_coef 1 --rho 0 --loss mae --diag >> out_run01.log &
+CUDA_VISIBLE_DEVICES=1 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 3 --nhid 64  --reg_coef 0 --mse_coef 1 --rho 0.001 --loss mae  >> out_run02.log &
+# CUDA_VISIBLE_DEVICES=2 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 5 --nhid 64  --reg_coef 0 --mse_coef 1 --rho 0.01 --loss mse  >> out_run03.log &
+# CUDA_VISIBLE_DEVICES=3 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 5 --nhid 64  --reg_coef 0 --mse_coef 1 --rho 0.001 --loss mse  >> out_run04.log &
 
 # CUDA_VISIBLE_DEVICES=0 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 3 --nhid 32  --reg_coef 0 --mse_coef 1 --rho 0 --nonlinearity softplus  >> out_run05.log &
 # CUDA_VISIBLE_DEVICES=1 nohup python train_resmix.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj --epochs 500 --batch_size 64 --num-rank 3 --nhid 32  --reg_coef 0 --mse_coef 1 --rho 0.001 --nonlinearity softplus  >> out_run06.log &
